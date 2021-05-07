414965eaae2594b80e58aec1f75c1d9bcc75a6b0
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-05-07 15:59:25 -0700

    Cleanup (#2)
    * Clear up
    
    * Clear out some more

--- src/IL4model.jl
+++ src/IL4model.jl
@@ -1,58 +1,33 @@
 module IL4model
 
-using ModelingToolkit, NLsolve
 import LinearAlgebra: norm
 using Optim
-
-const vars = @variables Lαβ Lαγ
-const params = @parameters L θ Kdα Kdβ Kdγ αT βT γT
-
-#const eqs = [0 ~ Lα*Kdα - α*L,
-#             0 ~ Lβ*Kdβ - β*L,
-#             0 ~ θ*Kdα*Lαβ - Lβ*α,
-#             #0 ~ θ*Kdβ*Lαβ - Lα*β,
-#             0 ~ α + Lα + Lαβ + Lαγ - αT,
-#             0 ~ Lα*(γT - Lαγ) - θ*Kdγ*Lαγ,
-#             0 ~ β + Lβ + Lαβ - βT]
-
-α = (αT - Lαβ - Lαγ) / (1 + L/Kdα)
-
-const eqs = [0 ~ θ*Kdα*Lαβ - (((βT - Lαβ) / (1 + L/Kdβ))*L/Kdβ)*α,
-             0 ~ (α*L/Kdα)*(γT - Lαγ) - θ*Kdγ*Lαγ]
-
-const ns = NonlinearSystem(eqs, collect(vars), collect(params))
-const nlsys_func = @eval eval(generate_function(ns)[2])
+using polyBindingModel
 
 
-""" Take in the parameters and calulate the solution for one condition.
-    Optionally provide a starting point for solving through u0. """
-function ligOut(ps::Vector{T}; u0 = T[0.01, 0.1])::Vector{T} where {T <: Real}
+""" Take in the parameters and calulate the solution for one condition. """
+function ligOut(ps::Vector{T})::T where {T <: Real}
     @assert all(ps .>= 0.0)
-    @assert length(ps) == 8
-    f2 = (du, u) -> nlsys_func(du, u, ps)
+    @assert length(ps) == 10
+    
+    # TODO: Add polyc here.
+    # ps: concentration, receptor affinities (3), receptor amounts (3), Kx, pSTAT weight, scaling
 
-    res = nlsolve(f2, u0, method = :newton, xtol = 1e-12, ftol = 1e-12, autodiff = :forward)
-    if !(res.x_converged | res.f_converged)
-        println(res)
-    end
-
-    @assert res.x_converged | res.f_converged
-    @assert all(res.zero .>= 0.0)
-    return res.zero
+    cplx = [0.0, 0.0]
+    return p[10] * (cplx[1] + p[9] * cplx[2])
 end
 
 """ Calculate a range of solutions. Ls defines the log10 concentration range. """
-function ligOutLs(Ls::LinRange, ps::Vector{T})::Matrix{T} where {T <: Real}
+function ligOutLs(Ls::LinRange, ps::Vector{T})::Vector{T} where {T <: Real}
     @assert all(ps .>= 0.0)
     @assert length(ps) == 7
     ps = copy(ps)
-    pushfirst!(ps, 10 ^ Ls[1])
-    results = zeros(T, length(Ls), length(vars))
-    results[1, :] = ligOut(ps)
+    pushfirst!(ps, 0.0)
+    results = zeros(T, length(Ls))
 
-    for ii in 2:length(Ls)
+    for ii in 1:length(Ls)
         ps[1] = 10 ^ Ls[ii]
-        results[ii, :] = ligOut(ps; u0 = results[ii - 1, :])
+        results[ii] = ligOut(ps)
     end
 
     return results
@@ -60,8 +35,8 @@ end
 
 """ Calculate the cost of the difference between the model and data.
     Species 1 is type II signaling, species 2 is type I signaling. """
-function diffcost(ps, c::LinRange, Y; species = 2)::Real
-    return norm(ligOutLs(c, ps)[:, species] .- Y)
+function diffcost(ps, c::LinRange, Y)::Real
+    return norm(ligOutLs(c, ps) .- Y)
 end
 
 """ Fit the two cell lines with two ligand responses. """
