761a27d95641581fe452177cb0b238d5a9d7b1dc
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-05-22 14:42:39 -0700

    Delete runtests.jl

--- test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using IL4model
-using Optim
-
-ligOut([1.0, 1.0, 1.0, 1.0, 10.0, 10.0, 0.1, 10.0])
-ligOutLs(concs, [1.0, 1.0, 1.0, 10.0, 10.0, 0.1, 10.0])
-
-res = optimizeParam()
-
-@assert Optim.minimum(res) < 100.0
