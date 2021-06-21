7be13d7962191f82ef7748996ac364ba1d74f977
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2021-06-21 09:03:06 -0700

    Updated Model (#11)

--- fitModel.py
+++ fitModel.py
@@ -2,64 +2,10 @@
 Implementation of a simple multivalent binding model.
 """
 
-import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
-from scipy.optimize import minimize
-from src.MBmodel import cytBindingModel, SigData
+from src.MBmodel import resids, fitFunc
 
-def fitFunc():
-    "Runs least squares fitting for various model parameters, and returns the minimizers"
-    x0 = np.array([-11, 0.5, 8.6, 8, 8.01, 7.6, 7, 7.01, 9.08, 8, 8.01, 8.59, 7, 7.01])  # KXSTAR, slopeT2, mIL4-IL4Ra, mIL4-Gamma, mIL4-IL13Ra, mNeo4-IL4Ra, mNeo4-Gamma, mNeo4-IL13Ra, hIL4-IL4Ra, hIL4-Gamma, hIL4-IL13Ra, hNeo4-IL4Ra, hNeo4-Gamma, hNeo4-IL13Ra (Log 10)
-    bnds = ((-14, -10), (0, 10), (4, 11), (4, 11), (4, 11), (4, 11), (4, 11), (4, 11), (4, 11), (4, 11), (4, 11), (4, 11), (4, 11), (4, 11))
-    parampredicts = minimize(resids, x0, method="L-BFGS-B", bounds=bnds, options={"disp": 999})
-    #parampredicts = minimize(resids, x0, method="trust-constr", bounds=bnds, options={"disp": 999}, constraints=cons)
-    assert parampredicts.success
-    return parampredicts.x
-
-def resids(x, retDF=False):
-    """"Returns residuals against signaling data"""
-    masterSTAT = pd.DataFrame(columns={"Cell", "Ligand", "Concentration", "Animal", "Experimental", "Predicted"})
-    Kx = x[0]
-    relRecp = x[1]
-    xPow = np.power(10, x)
-
-    CplxDict = {"mIL4": [xPow[2], xPow[3], xPow[4]],
-    "mNeo4": [xPow[5], xPow[6], xPow[7]],
-    "hIL4": [xPow[8], xPow[9], xPow[10]],
-    "hNeo4": [xPow[11], xPow[12], xPow[13]]}
-    if retDF:
-        SigDataFilt = SigData.loc[(SigData.Cell != "Macrophage") | (SigData.Animal != "Human")]
-    if not retDF:
-        SigDataFilt = SigData.loc[(SigData.Cell != "Macrophage") | (SigData.Animal != "Human")]
-    for cell in SigDataFilt.Cell.unique():
-        for animal in SigDataFilt.loc[SigData.Cell == cell].Animal.unique():
-            for ligand in SigDataFilt.loc[(SigDataFilt.Cell == cell) & (SigDataFilt.Animal == animal)].Ligand.unique():
-                isoData = SigDataFilt.loc[(SigDataFilt.Cell == cell) & (SigDataFilt.Animal == animal) & (SigDataFilt.Ligand == ligand)]
-                Concs = isoData.Concentration.values
-                normSigs = isoData.Signal.values
-                ligCplx = CplxDict[ligand]
-                if animal == "Human":
-                    results = cytBindingModel(Kx, ligCplx, Concs, cell, animal, relRecp)
-                else:
-                    results = cytBindingModel(Kx, ligCplx, Concs, cell, animal, relRecp)
-                masterSTAT = masterSTAT.append(pd.DataFrame({"Cell": cell, "Ligand": ligand, "Concentration": Concs, "Animal": animal, "Experimental": normSigs, "Predicted": results}))
-            
-            # Normalize
-            masterSTAT.loc[(masterSTAT.Cell == cell) & (masterSTAT.Animal == animal), "Predicted"] /= masterSTAT.loc[(masterSTAT.Cell == cell) & (masterSTAT.Animal == animal)].Predicted.max()
-            masterSTAT.loc[(masterSTAT.Cell == cell) & (masterSTAT.Animal == animal), "Experimental"] /= masterSTAT.loc[(masterSTAT.Cell == cell) & (masterSTAT.Animal == animal)].Experimental.max()
-    
-    masterSTAT = masterSTAT.fillna(0)
-    masterSTAT.replace([np.inf, -np.inf], 0, inplace=True)
-
-    if retDF:
-        return masterSTAT
-    else:
-        print(x)
-        print(np.linalg.norm(masterSTAT.Predicted.values - masterSTAT.Experimental.values))
-        return np.linalg.norm(masterSTAT.Predicted.values - masterSTAT.Experimental.values)
 
 xOpt = fitFunc()
 
--- src/MBmodel.py
+++ src/MBmodel.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from os.path import join
 from scipy.optimize import root
+from scipy.optimize import minimize, least_squares
 
 
 def Req_func2(Req: np.ndarray, L0, KxStar, Rtot: np.ndarray, Kav: np.ndarray):
@@ -51,16 +52,20 @@ def IL4mod(L0, KxStar, Rtot, Kav_in):
 
 path_here = pathlib.Path().absolute()
 recQuantDF = pd.read_csv(join(path_here, "src/data/RecQuant.csv"))
-SigData = pd.read_csv(join(path_here, "src/data/SignalingData.csv"))
 
 
-def cytBindingModel(Kx, Cplx, doseVec, cellType, animal, relRecp):
+def cytBindingModel(Kx, Cplx, doseVec, cellType, animal, relRecp, macIL4=False):
     """Runs binding model for a given mutein, valency, dose, and cell type."""
     doseVec = np.array(doseVec)
 
-    recCount = np.ravel([recQuantDF.loc[(recQuantDF.Receptor == "IL4Ra") & (recQuantDF["Cell"] == cellType) & (recQuantDF["Animal"] == animal)].Amount.values,
-                             recQuantDF.loc[(recQuantDF.Receptor == "Gamma") & (recQuantDF["Cell"] == cellType) & (recQuantDF["Animal"] == animal)].Amount.values,
-                             recQuantDF.loc[(recQuantDF.Receptor == "IL13Ra") & (recQuantDF["Cell"] == cellType) & (recQuantDF["Animal"] == animal)].Amount.values])
+    if not macIL4:
+        recCount = np.ravel([recQuantDF.loc[(recQuantDF.Receptor == "IL4Ra") & (recQuantDF["Cell"] == cellType) & (recQuantDF["Animal"] == animal)].Amount.values,
+                                recQuantDF.loc[(recQuantDF.Receptor == "Gamma") & (recQuantDF["Cell"] == cellType) & (recQuantDF["Animal"] == animal)].Amount.values,
+                                recQuantDF.loc[(recQuantDF.Receptor == "IL13Ra") & (recQuantDF["Cell"] == cellType) & (recQuantDF["Animal"] == animal)].Amount.values])
+    else:
+        recCount = np.ravel([np.power(10, macIL4),
+                                recQuantDF.loc[(recQuantDF.Receptor == "Gamma") & (recQuantDF["Cell"] == cellType) & (recQuantDF["Animal"] == animal)].Amount.values,
+                                recQuantDF.loc[(recQuantDF.Receptor == "IL13Ra") & (recQuantDF["Cell"] == cellType) & (recQuantDF["Animal"] == animal)].Amount.values])
 
     output = np.zeros([doseVec.size, 2])
 
@@ -68,3 +73,59 @@ def cytBindingModel(Kx, Cplx, doseVec, cellType, animal, relRecp):
         output[i, :] = IL4mod(np.power(10, dose), np.power(10, Kx), recCount, Cplx)
 
     return output[:, 0] +  output[:, 1] * relRecp
+
+
+def fitFunc():
+    "Runs least squares fitting for various model parameters, and returns the minimizers"
+    x0 = np.array([-11, 1, 8.6, 5, 5, 7.6, 5, 9.08, 5, 5, 8.59, 5, 2])  # KXSTAR, slopeT2, mIL4-IL4Ra, mIL4-Gamma, mIL4-IL13Ra, mNeo4-IL4Ra, mNeo4-Gamma, mNeo4-IL13Ra, hIL4-IL4Ra, hIL4-Gamma, hIL4-IL13Ra, hNeo4-IL4Ra, hNeo4-Gamma, hNeo4-IL13Ra (Log 10)
+    bnds = ([-14, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, -1], [-10, 10, 11, 6, 6, 11, 6, 11, 6, 6, 11, 6, 2.7])
+    parampredicts = least_squares(resids, x0, bounds=bnds)
+    #parampredicts = minimize(resids, x0, method="trust-constr", bounds=bnds, options={"disp": 999}, constraints=cons)
+    assert parampredicts.success
+    return parampredicts.x
+
+def resids(x, retDF=False):
+    """"Returns residuals against signaling data"""
+    SigData = pd.read_csv(join(path_here, "src/data/SignalingData.csv"))
+    masterSTAT = pd.DataFrame(columns={"Cell", "Ligand", "Concentration", "Animal", "Experimental", "Predicted"})
+    Kx = x[0]
+    relRecp = x[1]
+    xPow = np.power(10, x)
+
+    CplxDict = {"mIL4": [xPow[2], xPow[3], xPow[4]],
+    "mNeo4": [xPow[5], xPow[6], 1e2],
+    "hIL4": [xPow[7], xPow[8], xPow[9]],
+    "hNeo4": [xPow[10], xPow[11], 1e2]}
+    if not retDF:
+        SigData = SigData.loc[(SigData.Cell != "Macrophage") & (SigData.Cell != "Monocyte")]
+
+
+    for cell in SigData.Cell.unique():
+        for animal in SigData.loc[SigData.Cell == cell].Animal.unique():
+            for ligand in SigData.loc[(SigData.Cell == cell) & (SigData.Animal == animal)].Ligand.unique():
+                isoData = SigData.loc[(SigData.Cell == cell) & (SigData.Animal == animal) & (SigData.Ligand == ligand)]
+                Concs = isoData.Concentration.values
+                normSigs = isoData.Signal.values
+                ligCplx = CplxDict[ligand]
+                if animal == "Human":
+                    if cell == "Macrophage":
+                        results = cytBindingModel(Kx, ligCplx, Concs, cell, animal, relRecp, macIL4=x[12])
+                    else:
+                        results = cytBindingModel(Kx, ligCplx, Concs, cell, animal, relRecp)
+                else:
+                    results = cytBindingModel(Kx, ligCplx, Concs, cell, animal, relRecp)
+                masterSTAT = masterSTAT.append(pd.DataFrame({"Cell": cell, "Ligand": ligand, "Concentration": Concs, "Animal": animal, "Experimental": normSigs, "Predicted": results}))
+            
+            # Normalize
+            masterSTAT.loc[(masterSTAT.Cell == cell) & (masterSTAT.Animal == animal), "Predicted"] /= masterSTAT.loc[(masterSTAT.Cell == cell) & (masterSTAT.Animal == animal)].Predicted.max()
+            masterSTAT.loc[(masterSTAT.Cell == cell) & (masterSTAT.Animal == animal), "Experimental"] /= masterSTAT.loc[(masterSTAT.Cell == cell) & (masterSTAT.Animal == animal)].Experimental.max()
+    
+    masterSTAT = masterSTAT.fillna(0)
+    masterSTAT.replace([np.inf, -np.inf], 0, inplace=True)
+
+    if retDF:
+        return masterSTAT
+    else:
+        print(x)
+        print(np.linalg.norm(masterSTAT.Predicted.values - masterSTAT.Experimental.values))
+        return np.linalg.norm(masterSTAT.Predicted.values - masterSTAT.Experimental.values)
