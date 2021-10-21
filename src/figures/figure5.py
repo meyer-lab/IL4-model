0c6994f99baba392d64b4cb0789ff65dfbe2fc81
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2021-10-21 16:36:13 -0700

    Reformatted

--- src/figures/figure5.py
+++ src/figures/figure5.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from .figureCommon import getSetup
-from src.MBmodel import resids, R2_Plot_Cells, R2_Plot_Ligs, affFit, doseResponsePlot
+from src.MBmodel import resids, residsSeq, doseResponsePlot, R2_Plot_Mods
 
 rcParams['svg.fonttype'] = 'none'
 
@@ -15,19 +15,19 @@ rcParams['svg.fonttype'] = 'none'
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
 
-    ax, f = getSetup((10, 10), (3, 3))
+    ax, f = getSetup((10, 4), (1, 4))
 
     xOptimalMultnoGC = pd.read_csv("src/data/CurrentFitnoGC.csv").x.values
     resids(xOptimalMultnoGC, True, False, True)
     modelDFmulnoGC = resids(xOptimalMultnoGC, True, False)
-    modelDFmulnoGC = modelDFmulnoGC.loc[(modelDFmulnoGC.Ligand != "hIL13")]
+    modelDFmulnoGCnoIL13 = modelDFmulnoGC.loc[(modelDFmulnoGC.Ligand != "hIL13")]
 
-    R2_Plot_Cells(modelDFmulnoGC, ax[0:2], False, mice=True, training=True)
-    R2_Plot_Ligs(modelDFmulnoGC, ax[2], training=True)
-    R2_Plot_Cells(modelDFmulnoGC, ax[3:4], False, mice=False, training=False)
-    R2_Plot_Ligs(modelDFmulnoGC, ax[4], training=False)
-    affFit(ax[5], gcFit=False)
-    doseResponsePlot(ax[6:9], modelDFmulnoGC, allCells=False)
+    xOptimalSeqnoGC = pd.read_csv("src/data/CurrentFitSeqnoGC.csv").x.values
+    residsSeq(xOptimalSeqnoGC, True, False, True)
+    modelDFSeqnoGC = residsSeq(xOptimalSeqnoGC, True, False)
+
+    doseResponsePlot(ax[0:3], modelDFmulnoGCnoIL13, allCells=False)
+    R2_Plot_Mods(modelDFmulnoGC, modelDFSeqnoGC, ax=ax[3], training=False)
     plt.prism()
 
     return f
--- src/figures/figure6.py
+++ src/figures/figure6.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from .figureCommon import getSetup
-from src.MBmodel import doseResponsePlot, resids
+from src.MBmodel import doseResponsePlot, resids, residsSeq, affFit, affFitSeq, ABtestNorm
 
 rcParams['svg.fonttype'] = 'none'
 
@@ -15,11 +15,18 @@ rcParams['svg.fonttype'] = 'none'
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
 
-    ax, f = getSetup((10, 6), (2, 4))
+    ax, f = getSetup((10, 15), (5, 4))
 
     xOptimalMultnoGC = pd.read_csv("src/data/CurrentFitnoGC.csv").x.values
     modelDF = resids(xOptimalMultnoGC, retDF=True, gcFit=False, justPrimary=False)
+    doseResponsePlot(ax[0:8], modelDF=modelDF, model="Multivalent Model")
 
-    doseResponsePlot(ax, modelDF=modelDF)
+    xOptimalSeqnoGC = pd.read_csv("src/data/CurrentFitSeqnoGC.csv").x.values
+    modelDFSeq = residsSeq(xOptimalSeqnoGC, retDF=True, gcFit=False, justPrimary=False)
+    doseResponsePlot(ax[8:16], modelDF=modelDFSeq, model="Sequential Model")
+
+    affFit(ax[16], gcFit=False)
+    affFitSeq(ax[17:19], gcFit=False)
+    ABtestNorm(ax[19], xOptimalSeqnoGC, xOptimalMultnoGC)
 
     return f
